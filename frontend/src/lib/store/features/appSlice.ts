24893db318db0e7669d400b8cc6a411abbb1860a
Mahdad Ghasemian <mahdad.ghasemian@gmail.com>
2025-05-26 13:46:42 +0330

    Fixed AppSlice

--- frontend/src/lib/store/features/appSlice.ts
+++ frontend/src/lib/store/features/appSlice.ts
@@ -22,21 +22,96 @@ const initialState: AppState = {
     example: null,
     test: null,
   },
+  item: null,
 };
 
 export const appSlice = createSlice({
   name: "app",
   initialState,
   reducers: (create) => ({
-    setConrseContent: create.reducer((state, action: PayloadAction<string>) => {
+    setCourseContent: create.reducer((state, action: PayloadAction<string>) => {
       state.courseContent = action.payload;
     }),
+
+    setExampleContent: create.reducer(
+      (state, action: PayloadAction<string>) => {
+        state.exampleContent = action.payload;
+      }
+    ),
+
+    setTestQuestion: create.reducer((state, action: PayloadAction<string>) => {
+      state.testQuestion = action.payload;
+    }),
+
+    setCoursePattern: create.reducer(
+      (state, action: PayloadAction<AppState["coursePattern"]>) => {
+        state.coursePattern = action.payload;
+      }
+    ),
+
+    setExplanatoryChain: create.reducer(
+      (state, action: PayloadAction<AppState["explanatoryChain"]>) => {
+        state.explanatoryChain = action.payload;
+      }
+    ),
+
+    setTestSolution: create.reducer(
+      (state, action: PayloadAction<AppState["testSolution"]>) => {
+        state.testSolution = action.payload;
+      }
+    ),
+
+    setLoading: create.reducer(
+      (
+        state,
+        action: PayloadAction<{
+          section: keyof AppState["loading"];
+          loading: boolean;
+        }>
+      ) => {
+        state.loading[action.payload.section] = action.payload.loading;
+      }
+    ),
+
+    setError: create.reducer(
+      (
+        state,
+        action: PayloadAction<{
+          section: keyof AppState["errors"];
+          error: string | null;
+        }>
+      ) => {
+        state.errors[action.payload.section] = action.payload.error;
+      }
+    ),
+
+    resetState: create.reducer(() => initialState),
   }),
   selectors: {
-    getConrseContent: (state: AppState) => state.courseContent,
+    getCourseContent: (state) => state.courseContent,
+    getExampleContent: (state) => state.exampleContent,
+    getTestQuestion: (state) => state.testQuestion,
+    getLoading: (state) => state.loading,
+    getErrors: (state) => state.errors,
   },
 });
 
-export const { setConrseContent } = appSlice.actions;
+export const {
+  setCourseContent,
+  setExampleContent,
+  setTestQuestion,
+  setCoursePattern,
+  setExplanatoryChain,
+  setTestSolution,
+  setLoading,
+  setError,
+  resetState,
+} = appSlice.actions;
 
-export const { getConrseContent } = appSlice.selectors;
+export const {
+  getCourseContent,
+  getExampleContent,
+  getTestQuestion,
+  getLoading,
+  getErrors,
+} = appSlice.selectors;
